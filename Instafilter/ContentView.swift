6af78073a52dbcfad33be6e02bd040215a4b84ed
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-12 21:00:26 +0200

    Wrap a UIImagePickerController in a SwiftUI ImagePicker view

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -12,47 +12,23 @@ import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
+    @State private var showingImagePicker = false
     
     var body: some View {
         VStack {
             image?
                 .resizable()
                 .scaledToFit()
+            
+            Button("Select Image") {
+                self.showingImagePicker = true
+            }
         }
-        .onAppear(perform: loadImage)
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
     }
     
-    func loadImage() {
-        guard let inputImage = UIImage(named: "Example") else { return }
-        
-        let beginImage = CIImage(image: inputImage)
-        let context = CIContext()
-//        let currentFilter = CIFilter.sepiaTone()
-//        let currentFilter = CIFilter.pixellate()
-//        let currentFilter = CIFilter.crystallize()
-//        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-//        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-//        currentFilter.scale = 100
-//        currentFilter.radius = 200
-        // twirl distorsion
-        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
-        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
-        
-        
-        // get a CIImage from our filter or exit if that fails
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        // attempt to get a CGImage from our CIImage
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            // convert that to a UIImage
-            let uiImage = UIImage(cgImage: cgimg)
-            // and convert that to a SwiftUI image
-            image = Image(uiImage: uiImage)
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,21 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Dmitry Reshetnik on 12.03.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+}
