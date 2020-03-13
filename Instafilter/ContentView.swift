b1c0ca0a8197add533826f580661d6c86c337c8c
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-13 20:27:27 +0200

    Add Coordinator class to manage SwiftUI view controller

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -13,6 +13,7 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
     
     var body: some View {
         VStack {
@@ -24,9 +25,14 @@ struct ContentView: View {
                 self.showingImagePicker = true
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
     }
     
 }
