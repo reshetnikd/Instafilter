3aa06b2b4e77d44b1a96f587d233b4d468b64b26
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-15 15:27:13 +0200

    Refactor code base and add slider, to control each of the input keys

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -13,8 +13,11 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
     @State private var showingImagePicker = false
     @State private var showingFilterSheet = false
+    @State private var showingErrorAlert = false
     @State private var inputImage: UIImage?
     @State private var processedImage: UIImage?
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
@@ -27,6 +30,26 @@ struct ContentView: View {
             },
             set: {
                 self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        
+        let radius = Binding<Double>(
+            get: {
+                self.filterRadius
+            },
+            set: {
+                self.filterRadius = $0
+                self.applyProcessing()
+            }
+        )
+        
+        let scale = Binding<Double>(
+            get: {
+                self.filterScale
+            },
+            set: {
+                self.filterScale = $0
                 self.applyProcessing()
             }
         )
@@ -52,8 +75,64 @@ struct ContentView: View {
                 }
                 
                 HStack {
-                    Text("Intensity")
-                    Slider(value: intensity)
+                    if currentFilter.inputKeys.count > 2 {
+                        if currentFilter.inputKeys.contains(kCIInputIntensityKey) && currentFilter.inputKeys.contains(kCIInputRadiusKey) {
+                            VStack(alignment: .leading) {
+                                Text("Intensity")
+                                    .padding(10)
+                                Text("Radius")
+                                    .padding(10)
+                            }
+                            VStack {
+                                Slider(value: intensity)
+                                Slider(value: radius)
+                            }
+                        } else if currentFilter.inputKeys.contains(kCIInputIntensityKey) && currentFilter.inputKeys.contains(kCIInputScaleKey) {
+                            VStack {
+                                Text("Intensity")
+                                    .padding(10)
+                                Text("Scale")
+                                    .padding(10)
+                            }
+                            VStack {
+                                Slider(value: intensity)
+                                Slider(value: scale)
+                            }
+                        } else if currentFilter.inputKeys.contains(kCIInputRadiusKey) && currentFilter.inputKeys.contains(kCIInputScaleKey) {
+                            VStack {
+                                Text("Radius")
+                                    .padding(10)
+                                Text("Scale")
+                                    .padding(10)
+                            }
+                            VStack {
+                                Slider(value: radius)
+                                Slider(value: scale)
+                            }
+                        } else {
+                            if currentFilter.inputKeys.contains(kCIInputIntensityKey) {
+                                Text("Intensity")
+                                Slider(value: intensity)
+                            }
+                            if currentFilter.inputKeys.contains(kCIInputRadiusKey) {
+                                Text("Radius")
+                                Slider(value: radius)
+                            }
+                            if currentFilter.inputKeys.contains(kCIInputScaleKey) {
+                                Text("Scale")
+                                Slider(value: scale)
+                            }
+                        }
+                    } else if currentFilter.inputKeys.contains(kCIInputIntensityKey) {
+                        Text("Intensity")
+                        Slider(value: intensity)
+                    } else if currentFilter.inputKeys.contains(kCIInputRadiusKey) {
+                        Text("Radius")
+                        Slider(value: radius)
+                    } else {
+                        Text("Scale")
+                        Slider(value: scale)
+                    }
                 }.padding(.vertical)
                 
                 HStack {
@@ -64,7 +143,10 @@ struct ContentView: View {
                     Spacer()
                     
                     Button("Save") {
-                        guard let processedImage = self.processedImage else { return }
+                        guard let processedImage = self.processedImage else {
+                            self.showingErrorAlert = true
+                            return
+                        }
                         
                         let imageSaver = ImageSaver()
                         
@@ -86,7 +168,7 @@ struct ContentView: View {
                 ImagePicker(image: self.$inputImage)
             }
             .actionSheet(isPresented: $showingFilterSheet) {
-                ActionSheet(title: Text("Select a filter"), buttons: [
+                ActionSheet(title: Text("Select a filter\nCurrently selected filter: \(String(currentFilter.name.dropFirst(2)))"), buttons: [
                     .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
                     .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
                     .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
@@ -96,6 +178,9 @@ struct ContentView: View {
                     .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
                     .cancel()
                 ])
+            }
+            .alert(isPresented: $showingErrorAlert) {
+                Alert(title: Text("Error"), message: Text("There is no image to save."), dismissButton: .cancel())
             }
         }
     }
@@ -116,8 +201,8 @@ struct ContentView: View {
     func applyProcessing() {
         let inputKeys = currentFilter.inputKeys
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
         
